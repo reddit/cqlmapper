eb4ba527e2c0d1971d9b6c9b374af2f48ba751b8
Andrew Boyle <andrew.boyle@reddit.com>
2016-10-15 16:52:09 -0700

    statements tests don't need any changes

---
+++ tests/integration/statements/test_delete_statement.py
@@ -0,0 +1,91 @@
+# Copyright 2013-2016 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from unittest import TestCase
+
+from cqlmapper.columns import Column
+from cqlmapper.statements import DeleteStatement, WhereClause, MapDeleteClause, ConditionalClause
+from cqlmapper.operators import *
+import six
+
+
+class DeleteStatementTests(TestCase):
+
+    def test_single_field_is_listified(self):
+        """ tests that passing a string field into the constructor puts it into a list """
+        ds = DeleteStatement('table', 'field')
+        self.assertEqual(len(ds.fields), 1)
+        self.assertEqual(ds.fields[0].field, 'field')
+
+    def test_field_rendering(self):
+        """ tests that fields are properly added to the select statement """
+        ds = DeleteStatement('table', ['f1', 'f2'])
+        self.assertTrue(six.text_type(ds).startswith('DELETE "f1", "f2"'), six.text_type(ds))
+        self.assertTrue(str(ds).startswith('DELETE "f1", "f2"'), str(ds))
+
+    def test_none_fields_rendering(self):
+        """ tests that a '*' is added if no fields are passed in """
+        ds = DeleteStatement('table', None)
+        self.assertTrue(six.text_type(ds).startswith('DELETE FROM'), six.text_type(ds))
+        self.assertTrue(str(ds).startswith('DELETE FROM'), str(ds))
+
+    def test_table_rendering(self):
+        ds = DeleteStatement('table', None)
+        self.assertTrue(six.text_type(ds).startswith('DELETE FROM table'), six.text_type(ds))
+        self.assertTrue(str(ds).startswith('DELETE FROM table'), str(ds))
+
+    def test_where_clause_rendering(self):
+        ds = DeleteStatement('table', None)
+        ds.add_where(Column(db_field='a'), EqualsOperator(), 'b')
+        self.assertEqual(six.text_type(ds), 'DELETE FROM table WHERE "a" = %(0)s', six.text_type(ds))
+
+    def test_context_update(self):
+        ds = DeleteStatement('table', None)
+        ds.add_field(MapDeleteClause('d', {1: 2}, {1: 2, 3: 4}))
+        ds.add_where(Column(db_field='a'), EqualsOperator(), 'b')
+
+        ds.update_context_id(7)
+        self.assertEqual(six.text_type(ds), 'DELETE "d"[%(8)s] FROM table WHERE "a" = %(7)s')
+        self.assertEqual(ds.get_context(), {'7': 'b', '8': 3})
+
+    def test_context(self):
+        ds = DeleteStatement('table', None)
+        ds.add_where(Column(db_field='a'), EqualsOperator(), 'b')
+        self.assertEqual(ds.get_context(), {'0': 'b'})
+
+    def test_range_deletion_rendering(self):
+        ds = DeleteStatement('table', None)
+        ds.add_where(Column(db_field='a'), EqualsOperator(), 'b')
+        ds.add_where(Column(db_field='created_at'), GreaterThanOrEqualOperator(), '0')
+        ds.add_where(Column(db_field='created_at'), LessThanOrEqualOperator(), '10')
+        self.assertEqual(six.text_type(ds), 'DELETE FROM table WHERE "a" = %(0)s AND "created_at" >= %(1)s AND "created_at" <= %(2)s', six.text_type(ds))
+
+        ds = DeleteStatement('table', None)
+        ds.add_where(Column(db_field='a'), EqualsOperator(), 'b')
+        ds.add_where(Column(db_field='created_at'), InOperator(), ['0', '10', '20'])
+        self.assertEqual(six.text_type(ds), 'DELETE FROM table WHERE "a" = %(0)s AND "created_at" IN %(1)s', six.text_type(ds))
+
+        ds = DeleteStatement('table', None)
+        ds.add_where(Column(db_field='a'), NotEqualsOperator(), 'b')
+        self.assertEqual(six.text_type(ds), 'DELETE FROM table WHERE "a" != %(0)s', six.text_type(ds))
+
+    def test_delete_conditional(self):
+        where = [WhereClause('id', EqualsOperator(), 1)]
+        conditionals = [ConditionalClause('f0', 'value0'), ConditionalClause('f1', 'value1')]
+        ds = DeleteStatement('table', where=where, conditionals=conditionals)
+        self.assertEqual(len(ds.conditionals), len(conditionals))
+        self.assertEqual(six.text_type(ds), 'DELETE FROM table WHERE "id" = %(0)s IF "f0" = %(1)s AND "f1" = %(2)s', six.text_type(ds))
+        fields = ['one', 'two']
+        ds = DeleteStatement('table', fields=fields, where=where, conditionals=conditionals)
+        self.assertEqual(six.text_type(ds), 'DELETE "one", "two" FROM table WHERE "id" = %(0)s IF "f0" = %(1)s AND "f1" = %(2)s', six.text_type(ds))
---
+++ tests/integration/statements/test_update_statement.py
@@ -0,0 +1,90 @@
+# Copyright 2013-2016 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # noqa
+
+from cqlmapper.columns import Column, Set, List, Text
+from cqlmapper.operators import *
+from cqlmapper.statements import (UpdateStatement, WhereClause,
+                                  AssignmentClause, SetUpdateClause,
+                                  ListUpdateClause)
+import six
+
+
+class UpdateStatementTests(unittest.TestCase):
+
+    def test_table_rendering(self):
+        """ tests that fields are properly added to the select statement """
+        us = UpdateStatement('table')
+        self.assertTrue(six.text_type(us).startswith('UPDATE table SET'), six.text_type(us))
+        self.assertTrue(str(us).startswith('UPDATE table SET'), str(us))
+
+    def test_rendering(self):
+        us = UpdateStatement('table')
+        us.add_assignment(Column(db_field='a'), 'b')
+        us.add_assignment(Column(db_field='c'), 'd')
+        us.add_where(Column(db_field='a'), EqualsOperator(), 'x')
+        self.assertEqual(six.text_type(us), 'UPDATE table SET "a" = %(0)s, "c" = %(1)s WHERE "a" = %(2)s', six.text_type(us))
+
+        us.add_where(Column(db_field='a'), NotEqualsOperator(), 'y')
+        self.assertEqual(six.text_type(us), 'UPDATE table SET "a" = %(0)s, "c" = %(1)s WHERE "a" = %(2)s AND "a" != %(3)s', six.text_type(us))
+
+    def test_context(self):
+        us = UpdateStatement('table')
+        us.add_assignment(Column(db_field='a'), 'b')
+        us.add_assignment(Column(db_field='c'), 'd')
+        us.add_where(Column(db_field='a'), EqualsOperator(), 'x')
+        self.assertEqual(us.get_context(), {'0': 'b', '1': 'd', '2': 'x'})
+
+    def test_context_update(self):
+        us = UpdateStatement('table')
+        us.add_assignment(Column(db_field='a'), 'b')
+        us.add_assignment(Column(db_field='c'), 'd')
+        us.add_where(Column(db_field='a'), EqualsOperator(), 'x')
+        us.update_context_id(3)
+        self.assertEqual(six.text_type(us), 'UPDATE table SET "a" = %(4)s, "c" = %(5)s WHERE "a" = %(3)s')
+        self.assertEqual(us.get_context(), {'4': 'b', '5': 'd', '3': 'x'})
+
+    def test_additional_rendering(self):
+        us = UpdateStatement('table', ttl=60)
+        us.add_assignment(Column(db_field='a'), 'b')
+        us.add_where(Column(db_field='a'), EqualsOperator(), 'x')
+        self.assertIn('USING TTL 60', six.text_type(us))
+
+    def test_update_set_add(self):
+        us = UpdateStatement('table')
+        us.add_update(Set(Text, db_field='a'), set((1,)), 'add')
+        self.assertEqual(six.text_type(us), 'UPDATE table SET "a" = "a" + %(0)s')
+
+    def test_update_empty_set_add_does_not_assign(self):
+        us = UpdateStatement('table')
+        us.add_update(Set(Text, db_field='a'), set(), 'add')
+        self.assertFalse(us.assignments)
+
+    def test_update_empty_set_removal_does_not_assign(self):
+        us = UpdateStatement('table')
+        us.add_update(Set(Text, db_field='a'), set(), 'remove')
+        self.assertFalse(us.assignments)
+
+    def test_update_list_prepend_with_empty_list(self):
+        us = UpdateStatement('table')
+        us.add_update(List(Text, db_field='a'), [], 'prepend')
+        self.assertFalse(us.assignments)
+
+    def test_update_list_append_with_empty_list(self):
+        us = UpdateStatement('table')
+        us.add_update(List(Text, db_field='a'), [], 'append')
+        self.assertFalse(us.assignments)
