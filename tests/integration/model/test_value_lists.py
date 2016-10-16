889d268d4fb313e05ee03fefbc5e2918e6fa6748
Andrew Boyle <andrew.boyle@reddit.com>
2016-10-16 16:02:52 -0700

    test_value_lists

--- tests/integration/model/test_value_lists.py
+++ tests/integration/model/test_value_lists.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import random
-from tests.integration.cqlengine.base import BaseCassEngTestCase
+from tests.integration.base import BaseCassEngTestCase
 
 from cqlmapper.management import sync_table
 from cqlmapper.management import drop_table
@@ -24,25 +24,33 @@ from cqlmapper import columns
 class TestModel(Model):
 
     id = columns.Integer(primary_key=True)
-    clustering_key = columns.Integer(primary_key=True, clustering_order='desc')
+    clustering_key = columns.Integer(
+        primary_key=True,
+        clustering_order='desc',
+    )
+
 
 class TestClusteringComplexModel(Model):
 
     id = columns.Integer(primary_key=True)
-    clustering_key = columns.Integer(primary_key=True, clustering_order='desc')
+    clustering_key = columns.Integer(
+        primary_key=True,
+        clustering_order='desc',
+    )
     some_value = columns.Integer()
+
 
 class TestClusteringOrder(BaseCassEngTestCase):
 
     @classmethod
     def setUpClass(cls):
         super(TestClusteringOrder, cls).setUpClass()
-        sync_table(TestModel)
+        sync_table(cls.connection(), TestModel)
 
     @classmethod
     def tearDownClass(cls):
         super(TestClusteringOrder, cls).tearDownClass()
-        drop_table(TestModel)
+        drop_table(cls.connection(), TestModel)
 
     def test_clustering_order(self):
         """
@@ -51,25 +59,40 @@ class TestClusteringOrder(BaseCassEngTestCase):
         items = list(range(20))
         random.shuffle(items)
         for i in items:
-            TestModel.create(id=1, clustering_key=i)
+            TestModel.create(self.conn, id=1, clustering_key=i)
 
-        values = list(TestModel.objects.values_list('clustering_key', flat=True))
-        # [19L, 18L, 17L, 16L, 15L, 14L, 13L, 12L, 11L, 10L, 9L, 8L, 7L, 6L, 5L, 4L, 3L, 2L, 1L, 0L]
+        values = list(
+            TestModel.objects.values_list(
+                'clustering_key', flat=True
+            ).iter(self.conn)
+        )
+        # [19L, 18L, 17L, 16L, 15L, 14L, 13L, 12L, 11L, 10L, 9L, 8L, 7L, 6L,
+        #  5L, 4L, 3L, 2L, 1L, 0L]
         self.assertEqual(values, sorted(items, reverse=True))
 
     def test_clustering_order_more_complex(self):
         """
         Tests that models can be saved and retrieved
         """
-        sync_table(TestClusteringComplexModel)
+        sync_table(self.conn, TestClusteringComplexModel)
 
         items = list(range(20))
         random.shuffle(items)
         for i in items:
-            TestClusteringComplexModel.create(id=1, clustering_key=i, some_value=2)
+            TestClusteringComplexModel.create(
+                self.conn,
+                id=1,
+                clustering_key=i,
+                some_value=2,
+            )
 
-        values = list(TestClusteringComplexModel.objects.values_list('some_value', flat=True))
+        values = list(
+            TestClusteringComplexModel.objects.values_list(
+                'some_value',
+                flat=True,
+            ).iter(self.conn)
+        )
 
         self.assertEqual([2] * 20, values)
-        drop_table(TestClusteringComplexModel)
+        drop_table(self.conn, TestClusteringComplexModel)
 
