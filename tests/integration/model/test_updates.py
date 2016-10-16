e92d29710046c03a2f61a7ef91ecde0b2f1568f0
Andrew Boyle <andrew.boyle@reddit.com>
2016-10-16 15:58:05 -0700

    test_updates

--- tests/integration/model/test_updates.py
+++ tests/integration/model/test_updates.py
@@ -17,7 +17,7 @@ from uuid import uuid4
 from mock import patch
 from cqlmapper import ValidationError
 
-from tests.integration.cqlengine.base import BaseCassEngTestCase
+from tests.integration.base import BaseCassEngTestCase
 from cqlmapper.models import Model
 from cqlmapper import columns
 from cqlmapper.management import sync_table, drop_table
@@ -25,10 +25,10 @@ from cqlmapper.management import sync_table, drop_table
 
 class TestUpdateModel(Model):
 
-    partition   = columns.UUID(primary_key=True, default=uuid4)
-    cluster     = columns.UUID(primary_key=True, default=uuid4)
-    count       = columns.Integer(required=False)
-    text        = columns.Text(required=False, index=True)
+    partition = columns.UUID(primary_key=True, default=uuid4)
+    cluster = columns.UUID(primary_key=True, default=uuid4)
+    count = columns.Integer(required=False)
+    text = columns.Text(required=False, index=True)
 
 
 class ModelUpdateTests(BaseCassEngTestCase):
@@ -36,101 +36,130 @@ class ModelUpdateTests(BaseCassEngTestCase):
     @classmethod
     def setUpClass(cls):
         super(ModelUpdateTests, cls).setUpClass()
-        sync_table(TestUpdateModel)
+        sync_table(cls.connection(), TestUpdateModel)
 
     @classmethod
     def tearDownClass(cls):
         super(ModelUpdateTests, cls).tearDownClass()
-        drop_table(TestUpdateModel)
+        drop_table(cls.connection(), TestUpdateModel)
 
     def test_update_model(self):
         """ tests calling udpate on models with no values passed in """
-        m0 = TestUpdateModel.create(count=5, text='monkey')
+        m0 = TestUpdateModel.create(self.conn, count=5, text='monkey')
 
-        # independently save over a new count value, unknown to original instance
-        m1 = TestUpdateModel.get(partition=m0.partition, cluster=m0.cluster)
+        # independently save over a new count value, unknown to original
+        # instance
+        m1 = TestUpdateModel.get(
+            self.conn,
+            partition=m0.partition,
+            cluster=m0.cluster
+        )
         m1.count = 6
-        m1.save()
+        m1.save(self.conn)
 
         # update the text, and call update
         m0.text = 'monkey land'
-        m0.update()
+        m0.update(self.conn)
 
         # database should reflect both updates
-        m2 = TestUpdateModel.get(partition=m0.partition, cluster=m0.cluster)
+        m2 = TestUpdateModel.get(
+            self.conn,
+            partition=m0.partition,
+            cluster=m0.cluster,
+        )
         self.assertEqual(m2.count, m1.count)
         self.assertEqual(m2.text, m0.text)
 
     def test_update_values(self):
         """ tests calling update on models with values passed in """
-        m0 = TestUpdateModel.create(count=5, text='monkey')
+        m0 = TestUpdateModel.create(self.conn, count=5, text='monkey')
 
-        # independently save over a new count value, unknown to original instance
-        m1 = TestUpdateModel.get(partition=m0.partition, cluster=m0.cluster)
+        # independently save over a new count value, unknown to original
+        # instance
+        m1 = TestUpdateModel.get(
+            self.conn,
+            partition=m0.partition,
+            cluster=m0.cluster,
+        )
         m1.count = 6
-        m1.save()
+        m1.save(self.conn)
 
         # update the text, and call update
-        m0.update(text='monkey land')
+        m0.update(self.conn, text='monkey land')
         self.assertEqual(m0.text, 'monkey land')
 
         # database should reflect both updates
-        m2 = TestUpdateModel.get(partition=m0.partition, cluster=m0.cluster)
+        m2 = TestUpdateModel.get(
+            self.conn,
+            partition=m0.partition,
+            cluster=m0.cluster,
+        )
         self.assertEqual(m2.count, m1.count)
         self.assertEqual(m2.text, m0.text)
 
     def test_noop_model_direct_update(self):
-        """ Tests that calling update on a model with no changes will do nothing. """
-        m0 = TestUpdateModel.create(count=5, text='monkey')
+        """
+        Tests that calling update on a model with no changes will do nothing.
+        """
+        m0 = TestUpdateModel.create(self.conn, count=5, text='monkey')
 
-        with patch.object(self.session, 'execute') as execute:
-            m0.update()
+        with patch.object(self.conn.session, 'execute') as execute:
+            m0.update(self.conn)
         assert execute.call_count == 0
 
-        with patch.object(self.session, 'execute') as execute:
-            m0.update(count=5)
+        with patch.object(self.conn.session, 'execute') as execute:
+            m0.update(self.conn, count=5)
         assert execute.call_count == 0
 
         with self.assertRaises(ValidationError):
-            m0.update(partition=m0.partition)
+            m0.update(self.conn, partition=m0.partition)
 
         with self.assertRaises(ValidationError):
-            m0.update(cluster=m0.cluster)
+            m0.update(self.conn, cluster=m0.cluster)
 
     def test_noop_model_assignation_update(self):
         """ Tests that assigning the same value on a model will do nothing. """
         # Create object and fetch it back to eliminate any hidden variable
         # cache effect.
-        m0 = TestUpdateModel.create(count=5, text='monkey')
-        m1 = TestUpdateModel.get(partition=m0.partition, cluster=m0.cluster)
+        m0 = TestUpdateModel.create(self.conn, count=5, text='monkey')
+        m1 = TestUpdateModel.get(
+            self.conn,
+            partition=m0.partition,
+            cluster=m0.cluster,
+        )
 
-        with patch.object(self.session, 'execute') as execute:
-            m1.save()
+        with patch.object(self.conn.session, 'execute') as execute:
+            m1.save(self.conn)
         assert execute.call_count == 0
 
-        with patch.object(self.session, 'execute') as execute:
+        with patch.object(self.conn.session, 'execute') as execute:
             m1.count = 5
-            m1.save()
+            m1.save(self.conn)
         assert execute.call_count == 0
 
-        with patch.object(self.session, 'execute') as execute:
+        with patch.object(self.conn.session, 'execute') as execute:
             m1.partition = m0.partition
-            m1.save()
+            m1.save(self.conn)
         assert execute.call_count == 0
 
-        with patch.object(self.session, 'execute') as execute:
+        with patch.object(self.conn.session, 'execute') as execute:
             m1.cluster = m0.cluster
-            m1.save()
+            m1.save(self.conn)
         assert execute.call_count == 0
 
     def test_invalid_update_kwarg(self):
-        """ tests that passing in a kwarg to the update method that isn't a column will fail """
-        m0 = TestUpdateModel.create(count=5, text='monkey')
+        """
+        tests that passing in a kwarg to the update method that isn't a
+        column will fail
+        """
+        m0 = TestUpdateModel.create(self.conn, count=5, text='monkey')
         with self.assertRaises(ValidationError):
-            m0.update(numbers=20)
+            m0.update(self.conn, numbers=20)
 
     def test_primary_key_update_failure(self):
-        """ tests that attempting to update the value of a primary key will fail """
-        m0 = TestUpdateModel.create(count=5, text='monkey')
+        """
+        tests that attempting to update the value of a primary key will fail
+        """
+        m0 = TestUpdateModel.create(self.conn, count=5, text='monkey')
         with self.assertRaises(ValidationError):
-            m0.update(partition=uuid4())
+            m0.update(self.conn, partition=uuid4())
