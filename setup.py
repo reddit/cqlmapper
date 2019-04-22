c45820bae6236f90f65a9ca7ab1cf050538a654f
Jesse Li <jesseyli@users.noreply.github.com>
2019-04-22 14:16:39 -0700

    Cut version 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cqlmapper",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "cassandra-driver",
