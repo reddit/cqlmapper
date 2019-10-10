a22d9a8779c93897700457f93c6427f49ac2bd65
Andrew Boyle <pacejackson@users.noreply.github.com>
2019-10-09 17:21:42 -0700

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cqlmapper",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "cassandra-driver",
