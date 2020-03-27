0cb56af25ee5eb73daf0581762795e0c3b566390
Andrew Boyle <pacejackson@users.noreply.github.com>
2020-03-27 16:51:27 -0700

    Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cqlmapper",
-    version="0.2.2",
+    version="0.2.4",
     packages=find_packages(),
     install_requires=[
         "cassandra-driver",
